ca24455895821bf8ee2dd2cfa975c5e399e01d2c
Janak Bahadur Bohara <janakcustomx@gmail.com>
2024-06-26 09:44:25 +0545

    edit

--- script.js
+++ script.js
@@ -13,3 +13,29 @@ console.log(findBirthYear(age));
 
 //ab yasma maile user ko kunai use garer kehi garn skxu sir?
 // user.findBirthYear(age) 
+
+
+//**************************************************
+let password = req.body.password//"youtube123";
+
+let userSchema = {
+    name: String, 
+    password: String,
+    age: Number, 
+    address: String,
+    matchPassword: function(password) {//yo vaneko userSchema maa method create gare jastai ho ni sir?
+        let password2 = "janak2";
+        return password2 == password;
+    }
+
+}
+
+let user = await User. findOne({email});
+//user maa maanau user={name: "Jank", password: "hdfjkegjegedff0885", age: 32, address: "kanchanpur"} aayo vane
+
+user.matchPassword(password);//user.matchPasseord("youtube123") yo condition x vane user le function matchPassword sang kasto behaviour garx? or ki maile maathi lekheko code nai milen sir?
+
+
+
+
+
